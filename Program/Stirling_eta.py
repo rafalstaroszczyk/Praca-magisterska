55a5f783b0f883e89ac843f2be43be5193585081
Rafał Staroszczyk <staroszczykr@gmail.com>
2025-10-31 23:56:18 +0100

    Dodano Stirling bez regeneratora

--- Program/Stirling_eta.py
+++ Program/Stirling_eta.py
@@ -1,6 +1,8 @@
 import numpy as np
 import Stirling_harm_osc as sho
 import Stirling_spin_pol as ss2
+import Stirling_harm_osc_bezreg as shob
+import Stirling_spin_pol_bezreg as ss2b
 
 def StirlingHarmonicOscillator(omega1_min, omega1_max, \
         kappa_min, kappa_max, Tc, Th, grid_size):
@@ -32,6 +34,36 @@ def StirlingSpin2(omega1_min, omega1_max, \
     np.savetxt('Stirling_spin_pol_data.dat', eta, fmt='%.6f', \
         delimiter=' ', newline='\n')
 
+def StirlingHarmonicOscillatorBezReg(omega1_min, omega1_max, \
+        kappa_min, kappa_max, Tc, Th, grid_size):
+    omega1 = np.linspace(omega1_min, omega1_max, grid_size)
+    kappa = np.linspace(kappa_min, kappa_max, grid_size)
+    momega1, mkappa = np.meshgrid(omega1, kappa)
+    eta_grid = shob.eta_grid(momega1, mkappa, Tc, Th, grid_size)
+    eta = np.empty((grid_size**2,3))
+    for i in range(grid_size):
+        eta[grid_size * i:grid_size * (i+1), 0] = omega1[i]
+        for j in range(grid_size):
+            eta[grid_size * i + j, 1] = kappa[j]
+            eta[grid_size * i + j, 2] = eta_grid[j,i]
+    np.savetxt('Stirling_harm_osc_bezreg_data.dat', eta, fmt='%.6f', \
+        delimiter=' ', newline='\n')
+
+def StirlingSpin2BezReg(omega1_min, omega1_max, \
+        kappa_min, kappa_max, Tc, Th, grid_size):
+    omega1 = np.linspace(omega1_min, omega1_max, grid_size)
+    kappa = np.linspace(kappa_min, kappa_max, grid_size)
+    momega1, mkappa = np.meshgrid(omega1, kappa)
+    eta_grid = ss2b.eta_grid(momega1, mkappa, Tc, Th, grid_size)
+    eta = np.empty((grid_size**2,3))
+    for i in range(grid_size):
+        eta[grid_size * i:grid_size * (i+1), 0] = omega1[i]
+        for j in range(grid_size):
+            eta[grid_size * i + j, 1] = kappa[j]
+            eta[grid_size * i + j, 2] = eta_grid[j,i]
+    np.savetxt('Stirling_spin_pol_bezreg_data.dat', eta, fmt='%.6f', \
+        delimiter=' ', newline='\n')
+
 def main():
     grid_size = 51
     omega1_min = 0.1
@@ -44,6 +76,10 @@ def main():
     StirlingHarmonicOscillator(omega1_min, omega1_max, \
         kappa_min, kappa_max, Tc, Th, grid_size)
     StirlingSpin2(omega1_min, omega1_max, \
+        kappa_min, kappa_max, Tc, Th, grid_size)
+    StirlingHarmonicOscillatorBezReg(omega1_min, omega1_max, \
+        kappa_min, kappa_max, Tc, Th, grid_size)
+    StirlingSpin2BezReg(omega1_min, omega1_max, \
         kappa_min, kappa_max, Tc, Th, grid_size)
 
 if __name__ == "__main__":
---
+++ Program/Stirling_harm_osc_bezreg.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+def fU(T, omega):
+    return 0.5 * omega * 1/np.tanh(0.5 * omega / T)
+
+def fS(T, omega):
+    return 0.5 * omega / T * 1/np.tanh(0.5 * omega / T) - \
+        np.log(2 * np.sinh(0.5 * omega / T))
+
+def fQ12(omega1, kappa, Tc, Th):
+    return Tc * fS(Tc, kappa * omega1) - Tc * fS(Tc, omega1)
+
+def fQ23(omega1, kappa, Tc, Th):
+    return fU(Th, kappa * omega1) - fU(Tc, kappa * omega1)
+
+def fQ34(omega1, kappa, Tc, Th):
+    return Th * fS(Th, omega1) - Th * fS(Th, kappa * omega1)
+
+def fQ41(omega1, kappa, Tc, Th):
+    return fU(Tc, omega1) - fU(Th, omega1)
+
+def fW(Q12, Q23, Q34, Q41):
+    return Q12 + Q23 + Q34 + Q41
+
+def fQd(Q23, Q34):
+    return Q23 + Q34
+
+def feta(W, Qd):
+    return W/Qd
+
+def eta_grid(momega1, mkappa, Tc, Th, grid_size):
+    Q12 = fQ12(momega1, mkappa, Tc, Th)
+    Q23 = fQ23(momega1, mkappa, Tc, Th)
+    Q34 = fQ34(momega1, mkappa, Tc, Th)
+    Q41 = fQ41(momega1, mkappa, Tc, Th)
+    W = fW(Q12, Q23, Q34, Q41)
+    Qd = fQd(Q23, Q34)
+    eta = feta(W, Qd)
+    return eta
+
